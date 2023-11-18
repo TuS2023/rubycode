1dc500d578cb7088b65227c3d16e5d4e6d5a3eda
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-18 02:19:18 +0000

    代入

---
+++ ruby2.rb
@@ -0,0 +1,25 @@
+number = 100
+puts number + number
+puts number
+
+number = 100 * 20
+puts number
+
+number = 100 / 20
+puts number
+
+number = 100 % 30
+puts number
+
+number = 1234 * 5678 / 2
+puts number
+
+a = 31
+b = 17
+number = a * b 
+puts number
+
+x = 8
+y = 5
+number = x % y 
+puts number

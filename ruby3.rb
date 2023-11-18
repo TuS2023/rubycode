33d3ff5c4cd49503b53a92bb6e61ab32fd3b978a
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-18 02:27:01 +0000

    計算の優先順位

---
+++ ruby3.rb
@@ -0,0 +1,10 @@
+number = rand(1..10) * 100
+puts "体重#{number}キロのモンスターがあらわれた"
+
+number = (1234 + 5678) * 3
+puts number
+
+number = rand(1..10)
+money = 500 * number
+puts "スライムが#{number}匹あらわれた"
+puts "スライムを倒したときの獲得金額は#{money}円です"

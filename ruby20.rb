fcf394e237302ad335350a830257ddf197680042
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-19 13:13:09 +0000

    ループ処理

---
+++ ruby20.rb
@@ -0,0 +1,26 @@
+count = gets.to_i
+
+for i in 1..count
+    line = gets.chomp
+    puts "#{line}は、スライムを攻撃した！"
+end
+
+count = gets.to_i 
+for i in 1..count
+    line = gets
+    puts "スライムがあらわれた"
+end
+
+line = gets.to_i 
+count = gets.to_i  
+for i in line..count
+    puts i       
+    i += 1
+end
+
+count = gets.to_i  
+
+for i in 1..count
+    line = gets 
+    puts line
+end

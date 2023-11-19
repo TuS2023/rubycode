27df3ef393ec8fa704ad9e67cb0f60cb56f2ce02
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-19 05:28:49 +0000

    while

---
+++ ruby15.rb
@@ -0,0 +1,18 @@
+i = 1
+while i <= 10
+  puts i 
+  i = i + 1
+end
+puts "last#{i}"
+
+i = 1
+while i <= 5
+    puts "ハロー、paizaラーニング"
+    i = i + 1  
+end
+
+i = 0 
+while i <= 15
+    puts i      
+    i = i + 1
+end

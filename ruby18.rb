e4b8128b0440986b6f372749fa1f3dd4784bdaa7
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-19 09:00:44 +0000

    ループ処理

---
+++ ruby18.rb
@@ -0,0 +1,11 @@
+puts "<select name='age'>"
+for age in 1..100
+    puts "<option>#{age}才</option>"
+end
+puts "</select>"
+
+puts "<ul>"
+for i in 1..100
+    puts "<li>#{i}</li>"
+end
+puts "</ul>"

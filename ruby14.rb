b9e1e9b0d44cb400c20012b1bf3f4ccb3cb637b0
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-19 04:32:31 +0000

    for in

---
+++ ruby14.rb
@@ -0,0 +1,16 @@
+for i in 5..10
+  puts "hello world #{i}"
+end
+puts "last #{i}"
+
+for i in 1..5
+    puts "ハロー、paizaラーニング"
+end
+
+for i in 0..15
+    puts i      
+end
+
+for i in 1..12 
+    puts "#{i}月"
+end

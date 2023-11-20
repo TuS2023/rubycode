da41257805a86845a1a82cb35008e679b3966a09
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-20 06:13:08 +0000

    配列の基礎2

---
+++ ruby28.rb
@@ -0,0 +1,18 @@
+array =[]
+while line = gets 
+    line.chomp! 
+    array.push(line)
+end
+p array
+
+while line = gets
+	line.chomp!
+	puts "#{line}が現れた"
+end
+
+array = []
+while line = gets
+    line.chomp!
+    array.push(line)
+end
+p array

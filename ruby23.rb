f2e1b415c113fcc3783f8ffdbeba30f52d689d74
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-20 04:37:08 +0000

    配列からデータを取り出す

---
+++ ruby23.rb
@@ -0,0 +1,14 @@
+num = 0
+team = ["勇者", "魔法使い"]
+p team
+puts team[num + 1]
+p team[10]
+
+team = ["勇者", "戦士", "侍", "忍者", "魔法使い"]
+# teamの1番左の要素をputsメソッドで出力する
+puts team[0]
+
+team = ["戦士", "戦士", "侍", "忍者", "魔法使い"]
+# teamの3番目の要素をputsメソッドで出力する
+puts team[2]
+

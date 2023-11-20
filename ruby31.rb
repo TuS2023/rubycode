f4da8befa51bc70eedb7cbb0afa40ad4d837eeec
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-20 07:48:32 +0000

    ハッシュの要素

---
+++ ruby31.rb
@@ -0,0 +1,38 @@
+# ハッシュのおさらい
+enemies = {"ザコ" => "スライム", "中ボス" => "ドラゴン", "ラスボス" => "魔王"}
+p enemies
+puts enemies["中ボス"]
+puts enemies.length 
+
+enemies["ザコ2"] = "メタルモンスター"
+p enemies
+puts enemies.length
+
+enemies["中ボス"] = "レッドドラゴン"
+p enemies
+puts enemies.length
+
+enemies.delete("ザコ")
+p enemies
+puts enemies.length
+
+skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
+# この下で、ハッシュの長さを出力してみよう
+puts skills.length
+
+skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
+# この下で、ハッシュにデータを追加してみよう
+skills["属性"] = "炎"
+p skills
+
+skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
+p skills
+# この下で、ハッシュを更新してみよう
+skills["職業"] = "魔法使い"
+p skills
+
+skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
+p skills
+# この下で、ハッシュを削除してみよう
+skills.delete("体力")
+p skills

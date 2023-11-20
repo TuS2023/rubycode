7850479d82e1da43d57ba366a36c2726ba579db0
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-20 04:32:42 +0000

    配列

---
+++ ruby22.rb
@@ -0,0 +1,23 @@
+# 配列を作成する
+p1 = "戦士"
+p2 = "魔法使い"
+puts p1
+puts p2
+
+team = ["勇者", "魔法使い", 100, p1, p2]
+p team
+
+array = ["戦士","侍","僧侶","魔法使い"]
+# この下で、arrayを出力してみよう
+p array
+
+item = ["ロングソード","ブレードソード","エクスカリバー"]
+p item
+
+player_1 = "勇者"
+player_2 = "魔法使い"
+player_3 = "戦士"
+
+# player_1 ~ 3を、配列に代入して、pメソッドで出力してください。
+team = [player_1, player_2, player_3]
+p team

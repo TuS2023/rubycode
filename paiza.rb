089bc87eb119a4f55b7c9f9b0ab1e8bd6e57bc0f
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-21 03:00:26 +0000

    ランクC

--- paiza.rb
+++ paiza.rb
@@ -19,3 +19,43 @@ array3 = [1,2,3]
 array3 = array3.join(" ")
 p array3
 #=> "1 2 3"
+
+#半角スペース区切り出力
+n = gets.to_i
+
+arr = Array.new(n)
+n.times { |i| arr[i] = 'paiza' }
+
+puts arr.join(' ')
+
+#改行区切り出力
+n = gets.to_i
+
+a = gets.split(' ').map(&:to_i)
+puts a
+
+#値の追加と出力
+n = gets.to_i
+
+roster = Array.new(n)
+n.times { |i| roster[i] = gets.split(' ') }
+
+roster.each do |member|
+  name = member[0]
+  age = member[1].to_i
+
+  puts name + ' ' + (age + 1).to_s
+end
+#配列の要素数
+n = gets.to_i
+
+n.times { puts gets.chomp.length }
+#部分文字列
+a = gets.chomp
+s = gets
+
+if s.include? a
+  puts 'YES'
+else
+  puts 'NO'
+end

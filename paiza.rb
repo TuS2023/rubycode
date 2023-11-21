39ae4a75aad6db3e215a3cac8984d2cf5b763e1e
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-21 02:24:23 +0000

    join

--- paiza.rb
+++ paiza.rb
@@ -4,3 +4,18 @@ arr = Array.new(n) #配列に格納
 n.times { |i| arr[i] = gets.to_i } #配列を順番に整数を格納
 
 puts arr.max #maxで最大を出力
+
+array1 = [1,2,3]
+array1 = array.join
+p array1
+#=> "123"
+
+array2 = [1,2,3]
+array2 = array2.join("-")
+p array2
+#=> "1-2-3"
+
+array3 = [1,2,3]
+array3 = array3.join(" ")
+p array3
+#=> "1 2 3"
